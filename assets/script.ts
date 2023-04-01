8c7c93d7fca53b46c727cd10e0a1c9b547357eb2
Mahdi <mahdi.unic1@gmail.com>
2023-04-01 21:09:28 +0200

    stated with barsCountDown function- not finished yet

--- assets/script.ts
+++ assets/script.ts
@@ -1,20 +1,21 @@
 import Wolf from './Classes.ts/Wolf';
 import Dragon from './Classes.ts/Dragon';
 
-const homePageContainer = document.querySelector('.homePageContainer');
-const gameSectionContainer = document.querySelector('.gameSectionContainer');
+const homePageContainer: HTMLHRElement | null = document.querySelector('.homePageContainer');
+const gameSectionContainer: HTMLElement | null = document.querySelector('.gameSectionContainer');
+
 const resultContainer = document.querySelector('.resultContainer');
 
 
 // Home Page start button for starting the game
-const startButton = document.getElementById('homePageStartButton');
-const petsNameInput = document.getElementById('petsNameInput') as HTMLInputElement;
-const selectOption = document.getElementById('selectOption') as HTMLSelectElement;
+const startButton: HTMLElement | null = document.getElementById('homePageStartButton');
+const petsNameInput: HTMLInputElement = document.getElementById('petsNameInput') as HTMLInputElement;
+const selectOption: HTMLSelectElement = document.getElementById('selectOption') as HTMLSelectElement;
 
 startButton?.addEventListener('click', () => {
     
   if (petsNameInput.value === "" ) {
-    const NameInputErrorMessage = document.getElementById('NameInputErrorMessage');
+    const NameInputErrorMessage: HTMLElement | null = document.getElementById('NameInputErrorMessage');
     if (NameInputErrorMessage) {
       NameInputErrorMessage.innerText = 'Please enter your name';
       setTimeout(() => {
@@ -27,7 +28,7 @@ startButton?.addEventListener('click', () => {
       homePageContainer?.classList.add('hide');
       gameSectionContainer?.classList.remove('hide');
 
-      const dragonClass = new Dragon("People")
+      const dragonClass: Dragon = new Dragon("People")
       dragonClass.changeImage()
       dragonClass.dragonFoodButton()
       dragonClass.dragonBarIcon()
@@ -36,13 +37,50 @@ startButton?.addEventListener('click', () => {
       homePageContainer?.classList.add('hide');
       gameSectionContainer?.classList.remove('hide');
 
-      const wolfImage = new Wolf("rabbit")
+      const wolfImage: Wolf = new Wolf("rabbit")
       wolfImage.changeImage()
       wolfImage.wolfFoodButton()
       wolfImage.wolfBarIcon()
+
       
     } 
-
+    barsCountDown()
   }
+
 });
+
+
+// diminue de 10% entre 50% et 100% de point de vie
+// diminue de 6% entre 15% et 50% de point de vie
+// diminue de 2% entre 0% et 15% de point de vie
+// L’utilisateur peut alimenter les jauges à l’aide d’un bouton qui augmente la jauge de 10%, la jauge ne peut pas dépasser les 100%.
+
+// A function for decreacing the value of the bars every x minutes
+const barValues: NodeListOf<Element>  = document.querySelectorAll(".barValue") 
+
+function barsCountDown (){
+  let barValuesInnerText: number = 100
+
+  // First Interval timer for decreasing down to 50% by 10% each 2sec
+  let firstInterval: number ; 
+  if(barValuesInnerText <= 100 && barValuesInnerText >= 50){
+    firstInterval = setInterval(()=>{
+      barValuesInnerText -= 10 ; 
+      for(let i= 0; i < barValues.length; i++){
+        barValues[i].innerHTML = barValuesInnerText.toString()      
+      } 
+      if (barValuesInnerText === 50) {
+        clearInterval(firstInterval);
+      }
+    },1000)
+  }
+
+}
+
+
+
+
+
+
+// const barConuterElements = document.querySelectorAll(".barConuterElement")
 
